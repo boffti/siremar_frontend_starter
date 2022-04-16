73b57751eacc79edcf035876b26e9ee5ac3e24cb
Aneesh Melkot <boffti@gmail.com>
2022-04-16 13:46:35 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [benefitName, setBenefitName] = useState("");
+  const [benefits, setBenefits] = useState([]);
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    console.log(benefitName);
+
+    axios.post("http://localhost/siremar_backend/add_benefit.php", {
+      benefit: benefitName
+    }).then(res => {
+      console.log(res);
+      if(res.data.message === 'success') {
+        alert('Success');
+      } else {
+        alert('Failed');
+      }
+    })
+
+  }
+
+
+  const getBenefits = () => {
+    axios.get("http://localhost:8000/benefits")
+    .then(res => {
+      console.log(res.data);
+      setBenefits(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getBenefits();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-light bg-light container">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+        </div>
+      </nav>
+      <div className="container">
+        <form onSubmit={handelSubmit}>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Add benefit</label>
+            <input value={benefitName} onChange={e => setBenefitName(e.target.value)} name='benefit_name' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        <br></br>
+        {
+          benefits.map(function(benefit, idx) {
+            return (
+              <div key={idx}>
+                  <h1>{benefit.name}</h1>
+                  <hr></hr>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
